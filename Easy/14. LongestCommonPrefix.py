64bb875b64cf45b3d0185e3cdc22795ecfe64a54
chunz912 <zhangchun1991@gmail.com>
2019-01-13 21:36:25 -0800

    Update 14. LongestCommonPrefix.py

--- Easy/14. LongestCommonPrefix.py
+++ Easy/14. LongestCommonPrefix.py
@@ -12,3 +12,23 @@ def longestCommonPrefix(strs):
             
     else:
         return min(strs)
+
+    
+"""
+shorter version
+"""
+
+def longestCommonPrefix(self, strs):        
+        i = 0
+        for x in zip(*strs):            
+            if len(set(x)) > 1: return strs[0][:i]            
+            i += 1            
+        return strs[0][:i] if strs else ''
+    
+    
+"""
+already implemented in Python
+"""
+
+def longestCommonPrefix(self, strs):
+    return os.path.commonprefix(strs)
