a953932408f95fcb687288d8e9ab0717e3e3662a
chunz912 <zhangchun1991@gmail.com>
2019-01-16 23:07:11 -0800

    Add files via upload

---
+++ Easy/SearchInsertPosition.py
@@ -0,0 +1,14 @@
+"""
+solution
+"""
+
+def searchInsert(nums, target):
+    if target < nums[0]:
+        return 0
+    elif target > nums[-1]:
+        return len(nums)
+    for i in range(len(nums)):
+        if nums[i] == target:
+            return i
+        elif nums[i] < target and target < nums[i+1]:
+            return i+1
