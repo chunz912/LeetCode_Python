f5f174401a3d87c1dd39d747ddc29a8ea927363a
chunz912 <zhangchun1991@gmail.com>
2019-01-26 14:47:00 -0800

    Update and rename PlusOne.py to 66. PlusOne.py

--- Easy/66. PlusOne.py
+++ Easy/66. PlusOne.py
@@ -12,3 +12,13 @@ def PlusOne(digits):
         digit = int(new_num[j])
         out.append(digit)
     return out
+
+"""
+solution
+"""
+def PlusOne(digits):
+    num = 0
+    for i in range(len(digits)):
+        num += digits[i] * pow(10, len(digits)-1-i)
+    return [int(1) for i in str(num + 1)]
+
