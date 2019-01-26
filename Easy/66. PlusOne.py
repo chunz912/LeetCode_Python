554efca441e68323eefc188f3606eb4e50bcd82e
chunz912 <zhangchun1991@gmail.com>
2019-01-26 14:48:38 -0800

    Update 66. PlusOne.py

--- Easy/66. PlusOne.py
+++ Easy/66. PlusOne.py
@@ -15,6 +15,12 @@ def PlusOne(digits):
 
 """
 solution
+We're given a list of digits, and the idea here is to convert that list to an integer, num. 
+So each digit is multiplied by the proper place value and added to num. 
+For example, if digits = [3, 8, 2, 5] then on the first iteration 3 is multiplied by 10 to the power of 4-1-0 = 3, 
+so this results in 3000, which is added to num. Then 8 is multiplied by 10^2 and added to num, and so on.
+
+The last step is to add 1 to num, convert it to a list and return that list.
 """
 def PlusOne(digits):
     num = 0
